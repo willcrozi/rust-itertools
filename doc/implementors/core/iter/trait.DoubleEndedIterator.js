bdd6f4a140de46377a6bf0708ed6b9325d3863ce
root <root@localhost>
2014-07-29 00:39:17 +0200

    Add docs

---
+++ doc/implementors/core/iter/trait.DoubleEndedIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['itertools'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;A, B, I: <a class='trait' href='http://doc.rust-lang.org/master/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a>&lt;A&gt;&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a>&lt;B&gt; for <a class='struct' href='itertools/struct.FnMap.html' title='itertools::FnMap'>FnMap</a>&lt;A, B, I&gt;","<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, T&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a>&lt;&amp;'a T&gt; for <a class='struct' href='itertools/struct.Stride.html' title='itertools::Stride'>Stride</a>&lt;'a, T&gt;","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a>&lt;<a href='http://doc.rust-lang.org/master/std/primitive.uint.html'>uint</a>&gt; for <a class='struct' href='itertools/struct.Times.html' title='itertools::Times'>Times</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/core/ops/trait.Index.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['itertools'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, T&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.Index.html' title='core::ops::Index'>Index</a>&lt;<a href='http://doc.rust-lang.org/master/std/primitive.uint.html'>uint</a>, T&gt; for <a class='struct' href='itertools/struct.Stride.html' title='itertools::Stride'>Stride</a>&lt;'a, T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
