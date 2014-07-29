f0c28d6d49deeb0e244bd711edc7eb1bba19e92d
root <root@localhost>
2014-07-29 22:55:56 +0200

    Update gh-pages

--- doc/implementors/core/fmt/trait.Show.js
+++ doc/implementors/core/fmt/trait.Show.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['itertools'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, T: <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a>&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='itertools/struct.Stride.html' title='itertools::Stride'>Stride</a>&lt;'a, T&gt;",];
+implementors['itertools'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, A: <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a>&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='itertools/struct.Stride.html' title='itertools::Stride'>Stride</a>&lt;'a, A&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
