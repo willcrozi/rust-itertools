fb404708be5cea3846fcfcc47d9e89328832a1cf
root <root@localhost>
2014-08-19 19:56:31 +0200

    Update docs

---
+++ doc/implementors/core/ops/trait.IndexMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['itertools'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, A&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.IndexMut.html' title='core::ops::IndexMut'>IndexMut</a>&lt;<a href='http://doc.rust-lang.org/master/std/primitive.uint.html'>uint</a>, A&gt; for <a class='struct' href='itertools/struct.StrideMut.html' title='itertools::StrideMut'>StrideMut</a>&lt;'a, A&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
