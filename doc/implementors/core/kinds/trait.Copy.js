5e4939d7704097b2866cd7da283f09c412c2054c
root <root@localhost>
2014-12-13 11:30:00 +0100

    Update docs

---
+++ doc/implementors/core/kinds/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['itertools'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, A&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/kinds/trait.Copy.html' title='core::kinds::Copy'>Copy</a> for <a class='struct' href='itertools/struct.Stride.html' title='itertools::Stride'>Stride</a>&lt;'a, A&gt;","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/kinds/trait.Copy.html' title='core::kinds::Copy'>Copy</a> for <a class='struct' href='itertools/struct.Times.html' title='itertools::Times'>Times</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
