8ec48330ce73c795db758a3bfaedb2b53422dc0b
root <root@localhost>
2015-01-10 03:18:32 +0100

    Update docs

--- doc/implementors/core/iter/trait.RandomAccessIterator.js
+++ doc/implementors/core/iter/trait.RandomAccessIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['itertools'] = ["<a class='stability Experimental' title='Experimental: not widely used, may be better decomposed into Index and ExactSizeIterator'></a>impl&lt;A, B, T, U&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.RandomAccessIterator.html' title='core::iter::RandomAccessIterator'>RandomAccessIterator</a> for <a class='struct' href='itertools/struct.ZipLongest.html' title='itertools::ZipLongest'>ZipLongest</a>&lt;T, U&gt;",];
+implementors['itertools'] = ["<a class='stability Unstable' title='Unstable: not widely used, may be better decomposed into Index and ExactSizeIterator'></a>impl&lt;A, B, T, U&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.RandomAccessIterator.html' title='core::iter::RandomAccessIterator'>RandomAccessIterator</a> for <a class='struct' href='itertools/struct.ZipLongest.html' title='itertools::ZipLongest'>ZipLongest</a>&lt;T, U&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/core/ops/trait.IndexMut.js
+++ doc/implementors/core/ops/trait.IndexMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['itertools'] = ["<a class='stability Experimental' title='Experimental'></a>impl&lt;'a, A&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.IndexMut.html' title='core::ops::IndexMut'>IndexMut</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.uint.html'>uint</a>&gt; for <a class='struct' href='itertools/struct.StrideMut.html' title='itertools::StrideMut'>StrideMut</a>&lt;'a, A&gt;",];
+implementors['itertools'] = ["<a class='stability Unstable' title='Unstable'></a>impl&lt;'a, A&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.IndexMut.html' title='core::ops::IndexMut'>IndexMut</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.usize.html'>usize</a>&gt; for <a class='struct' href='itertools/struct.StrideMut.html' title='itertools::StrideMut'>StrideMut</a>&lt;'a, A&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
