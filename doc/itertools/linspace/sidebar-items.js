12264f28759a9c12dea4afbb7ebc6e0f3eff66ff
root <root@localhost>
2015-04-02 21:31:41 +0200

    Update docs

--- doc/itertools/linspace/sidebar-items.js
+++ doc/itertools/linspace/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["linspace","Return an iterator with `n` elements, where the first element is `a` and the last element is `b`."]],"struct":[["Linspace","An iterator of a sequence of evenly spaced floats."]]});
+initSidebarItems({"struct":[["Linspace","An iterator of a sequence of evenly spaced floats."]],"fn":[["linspace","Return an iterator with `n` elements, where the first element is `a` and the last element is `b`."]]});
--- doc/itertools/times/sidebar-items.js
+++ doc/itertools/times/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Times","Iterator to repeat a simple number of times"]],"fn":[["times","Return an iterator with `n` elements, for simple repetition a particular number of times. The iterator yields a counter."]]});
+initSidebarItems({"fn":[["times","Return an iterator with `n` elements, for simple repetition a particular number of times. The iterator yields a counter."]],"struct":[["Times","Iterator to repeat a simple number of times"]]});
