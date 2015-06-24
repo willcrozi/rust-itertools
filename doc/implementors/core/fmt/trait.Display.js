34485b3d94bb550a1b65fbc7dfce4d2b5c536e27
root <root@localhost>
2015-06-24 21:45:06 +0200

    Update docs

---
+++ doc/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['itertools'] = ["impl&lt;'a, I, F&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='itertools/struct.Format.html' title='itertools::Format'>Format</a>&lt;'a, I, F&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
