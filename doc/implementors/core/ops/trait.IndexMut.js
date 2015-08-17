8d1397c8c563aaf283b0d0cbe29c71ab31bd2e28
bluss <bluss@users.noreply.github.com>
2015-08-17 03:08:53 +0200

    Update docs

--- doc/implementors/core/ops/trait.IndexMut.js
+++ doc/implementors/core/ops/trait.IndexMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['itertools'] = ["impl&lt;'a, A&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.IndexMut.html' title='core::ops::IndexMut'>IndexMut</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.usize.html'>usize</a>&gt; for <a class='struct' href='itertools/struct.StrideMut.html' title='itertools::StrideMut'>StrideMut</a>&lt;'a, A&gt;",];
+implementors['itertools'] = ["impl&lt;'a, A&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.IndexMut.html' title='core::ops::IndexMut'>IndexMut</a>&lt;<a href='https://doc.rust-lang.org/nightly/std/primitive.usize.html'>usize</a>&gt; for <a class='struct' href='itertools/struct.StrideMut.html' title='itertools::StrideMut'>StrideMut</a>&lt;'a, A&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
